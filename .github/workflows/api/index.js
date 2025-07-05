a4b1fe2ab908d2b96f40b255ef08bfb2c9e78333
mahyarbr96 <mahyarbr96@gmail.com>
2025-07-05 12:16:08 +0330

    Delete .github/workflows/api directory

--- .github/workflows/api/index.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const url = 'https://cdo.iranesabz.workers.dev' + req.url;
-  const response = await fetch(url);
-  const contentType = response.headers.get('content-type');
-  const body = await response.text();
-
-  res.setHeader('Content-Type', contentType || 'text/html');
-  res.status(response.status).send(body);
-}
