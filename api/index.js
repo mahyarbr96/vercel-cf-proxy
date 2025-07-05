6eb473a5c513ed53f8f6bac71bf951d80962aee3
mahyarbr96 <mahyarbr96@gmail.com>
2025-07-05 12:53:09 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ export default async function handler(req, res) {
   const response = await fetch(url);
   const contentType = response.headers.get('content-type');
   const body = await response.text();
-
   res.setHeader('Content-Type', contentType || 'text/html');
   res.status(response.status).send(body);
 }
